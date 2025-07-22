f2ec0d35408a01c1281b3f97054200f56c73864f
Remruata <erp@mzhssp.in>
2025-07-22 13:52:30 +0530

    making chatbox bigger2

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -120,8 +120,8 @@ export default function AdminLayout({
 					</div>
 				</div>
 
-				<main className="flex-1 relative z-0 overflow-y-auto focus:outline-none py-6">
-					<div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+				<main className="flex-1 relative z-0 overflow-y-auto focus:outline-none py-2">
+					<div className="w-full px-2">
 						{children}
 					</div>
 				</main>
